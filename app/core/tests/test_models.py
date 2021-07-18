70eac84082f8e70c079f468dea511e24d510d3ff
lu bowen <reginold.lu@gmail.com>
2021-07-18 17:17:38 +0900

    fix: user model test failed

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,15 +1,15 @@
-from django.test import Testcase
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(Testcase):
+class ModelTests(TestCase):
 
     def test_create_user_with_email_sucessful(self):
         email = "abc@gmail.com"
         password = "test123"
-        user = get_user_model().obejects.create_user(
-            email=email,
-            password=password
-        )
+        user = get_user_model().objects.create_user(
+			email=email,
+			password=password
+		)
 
-        self.assertEqual(user.email. email)
-        self.assertTrue(user.check_password(password))        
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
