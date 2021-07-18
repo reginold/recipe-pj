0aa637698dac6ce39d812656a77f2ea66d676cac
lu bowen <reginold.lu@gmail.com>
2021-07-18 18:50:00 +0900

    feature&test: add the super user and emali format check

--- app/core/models.py
+++ app/core/models.py
@@ -9,9 +9,19 @@ from django.db import models
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
+        if not email:
+            raise ValueError("User must have an email")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         # save the model
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,13 +6,32 @@ class ModelTests(TestCase):
     def test_create_user_with_email_sucessful(self):
         email = "abc@gmail.com"
         password = "test123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_user_email_normalize(self):
         email = "abc@GMAILCOM"
-        user = get_user_model().objects.create_user(email=email, password="test123")
+        user = get_user_model().objects.create_user(
+            email=email,
+            password="test123"
+        )
 
         self.assertEqual(user.email, email.lower())
+
+    def test_user_mail_valid(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+    def test_is_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            "test@gmail.com",
+            "test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
