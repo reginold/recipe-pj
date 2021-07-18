9e22720ea3901745710849272c09e605c4d08709
lu bowen <reginold.lu@gmail.com>
2021-07-18 17:37:08 +0900

    config: add the workspace

--- recipe-pj/app/core/tests/test_models.py
+++ recipe-pj/app/core/tests/test_models.py
@@ -7,9 +7,9 @@ class ModelTests(TestCase):
         email = "abc@gmail.com"
         password = "test123"
         user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+            email=email,
+            password=password
+            )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
