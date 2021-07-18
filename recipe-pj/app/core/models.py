95d5cdd214fb708df166b0e476f39a57503f7d7a
lu bowen <reginold.lu@gmail.com>
2021-07-18 17:52:13 +0900

    fix: move the travis file

--- recipe-pj/app/core/models.py
+++ recipe-pj/app/core/models.py
@@ -1,15 +1,17 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-	# save the model
+        # save the model
         user.save(using=self._db)
 
         return user
@@ -17,6 +19,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -24,4 +27,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
