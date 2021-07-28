f6fafc7566a3452247c5040ad042b231b0d2027b
lu bowen <reginold.lu@gmail.com>
2021-07-28 17:08:07 +0900

    feature: add the tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,14 +1,10 @@
+from core.models import Tag
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 from django.test import TestCase
-
+from django.urls import reverse
+from recipe.serializers import TagSerializer
 from rest_framework import status
 from rest_framework.test import APIClient
-
-from core.models import Tag
-
-from recipe.serializers import TagSerializer
-
 
 TAGS_URL = reverse('recipe:tag-list')
 
@@ -63,3 +59,21 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Simple'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
